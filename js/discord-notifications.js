d001074dffeb3991bacdf4b7f417144798017744
LttlMario <bmariomihail@gmail.com>
2026-07-29 20:45:43 +0300

    Add files via upload

--- js/discord-notifications.js
+++ js/discord-notifications.js
@@ -1,6 +1,6 @@
 (() => {
     const SUPABASE_URL = 'https://vkvsabbbawyiurnaiugo.supabase.co';
-    const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ2a3ZzYWJiYmF3eWl1cm5haXVnbyIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzg1MDQ2OTU2LCJleHAiOjIxMDA2MjI5NTZ9.s2R8mdsoeQzOyz7a-NJOi5SQmyDmyy98cfUMfGqLo44';
+    const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrdnNhYmJiYXd5aXVybmFpdWdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwNDY5NTYsImV4cCI6MjEwMDYyMjk1Nn0.s2R8mdsoeQzOyz7a-NJOi5SQmyDmyy98cfUMfGqLo44';
     window.sendPanelDiscord = async (channel, payload) => {
         const accessToken = localStorage.getItem('discord_access_token');
         if (!accessToken) throw new Error('Sesiunea Discord lipsește. Autentifică-te din nou.');
