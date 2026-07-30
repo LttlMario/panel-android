0574f77df739cde6abc87aa3cd8f50afb591a4a6
LttlMario <bmariomihail@gmail.com>
2026-07-30 09:09:13 +0300

    Sync footer fallback to version 3.0.0

--- js/global-footer.js
+++ js/global-footer.js
@@ -8,7 +8,7 @@
   });
 
   function getReleaseVersion() {
-    return window.PANEL_RELEASE?.version || "2.9.0";
+    return window.PANEL_RELEASE?.version || "3.0.0";
   }
 
   function removeLegacySupportElements() {
