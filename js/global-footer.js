b7a07e503b804eed18e7e0553b7dc741a8e400c0
LttlMario <LttlMario@users.noreply.github.com>
2026-08-01 20:00:47 +0300

    Release Android 1.0.5 origine clara roluri Discord

--- js/global-footer.js
+++ js/global-footer.js
@@ -10,7 +10,7 @@
   });
 
   function getReleaseVersion() {
-    return window.PANEL_RELEASE?.version || "3.2.6";
+    return window.PANEL_RELEASE?.version || "3.2.7";
   }
 
   function removeLegacySupportElements() {
